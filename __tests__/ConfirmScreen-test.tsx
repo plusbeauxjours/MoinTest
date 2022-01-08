eedb99a8010e5fd39bd0ea9962deeedd12c46c93
🫒plusbeauxjours <plusbeauxjours@gmail.com>
2022-01-08 17:24:48 +0700

    set: test init

---
+++ __tests__/ConfirmScreen-test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import MainScreen from '../src/screens/MainScreen';
+
+describe('MainScreen', () => {
+    it('renders', () => {});
+});
---
+++ __tests__/MainScreen-test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Renderer from 'react-test-renderer';
+import ConfirmScreen from '../src/screens/ConfirmScreen';
+
+describe('ConfirmScreen', () => {
+    it('renders', () => {});
+});
